3fbc39e48e94bee398064137248767c35cc83f94
Simon Ratner <simon+github@probablyprime.net>
2011-05-28 14:49:21 -0700

    win32 service, documentation

---
+++ README.md
@@ -0,0 +1,22 @@
+# Installation #
+This is a Win32 service; to install:
+    python doomsday.py --username <user> --password <pass> install
+
+User account must have `SeServiceLogonRight` security access to start and stop the service, see [KB325349](http://support.microsoft.com/kb/325349/en-us/ "How to grant users rights to manage services").
+
+# Usage #
+To start the service and configure it to execute a command when triggered:
+    sc start DoomsdayDevice <command>
+
+Due to [Session 0 isolation](http://msdn.microsoft.com/en-us/library/bb756986.aspx), command will never display any UI, even when the process is running as the logged in user.
+
+To configure the service to start on system boot:
+    sc config DoomsdayDevice start= auto
+
+To stop the service:
+    sc stop DoomsdayDevice
+
+To uninstall:
+    sc stop DoomsdayDevice
+    sc delete DoomsdayDevice
+
--- doomsday.py
+++ doomsday.py
@@ -1,42 +1,75 @@
-""" Record a few seconds of audio and save to a WAVE file. """
+""" Monitor microphone for noise. """
 
 from collections import deque
 import math
-import signal
 import struct
+
 import pyaudio
 
-abort = False
+from threading import Thread, Event
+import win32api
+import win32event
+import win32service
+import win32serviceutil
+import servicemanager
 
-def handler(signum, frame):
-  global abort
-  abort = True
-  print "* interrupted"
-  signal.signal(signal.SIGINT, signal.SIG_DFL)
+def listen(signal, terminate):
+  rate = 44100
+  audio = pyaudio.PyAudio()
+  samples = deque(maxlen = 10 * rate)
 
-signal.signal(signal.SIGINT, handler)
+  stream = audio.open(input = True,
+                      format = pyaudio.paInt16,
+                      channels = 1,
+                      rate = rate,
+                      frames_per_buffer = 1024)
+  while not terminate.is_set():
+    try:
+      data = struct.unpack("%dh" % rate, stream.read(rate))
+    except IOError:
+      pass
+    else:
+      samples.extend(data)
+      rms = math.sqrt(sum(n*n for n in samples) / len(samples))
+      if rms > 20000:
+        samples.clear()
+        signal.set()
+  stream.close()
+  audio.terminate()
 
-rate = 44100
-audio = pyaudio.PyAudio()
-samples = deque(maxlen = 10 * rate)
+class DoomsdayDevice(win32serviceutil.ServiceFramework):
+  _svc_name_ = "DoomsdayDevice"
+  _svc_display_name_ = "Doomsday Device"
 
-stream = audio.open(input = True,
-                    format = pyaudio.paInt16,
-                    channels = 1,
-                    rate = rate,
-                    frames_per_buffer = 1024)
-print "* listening"
-while not abort:
-  try:
-    data = struct.unpack("%dh" % rate, stream.read(rate))
-  except IOError:
-    print "* warning: dropped frames"
-  else:
-    samples.extend(data)
-    rms = math.sqrt(sum(n*n for n in samples) / len(samples))
-    if rms > 20000:
-      samples.clear()
-      print "BOOM!"
+  def __init__(self, args):
+    win32serviceutil.ServiceFramework.__init__(self, args)
+    self.terminate = Event()
+    self.signal = Event()
+    self.cmd = " ".join(args[1:])
 
-stream.close()
-audio.terminate()
+  def log(self, msg):
+    servicemanager.LogInfoMsg(msg)
+
+  def warn(self, msg):
+    servicemanager.LogWarningMsg(msg)
+
+  def error(self, msg):
+    servicemanager.LogErrorMsg(msg)
+
+  def SvcStop(self):
+    self.terminate.set()
+
+  def SvcDoRun(self):
+    worker = Thread(target = listen, args = (self.signal, self.terminate))
+    worker.start()
+    while not self.terminate.is_set():
+      if self.signal.wait(1):
+        self.signal.clear()
+        self.log("Triggered (command=%s)" % self.cmd)
+        if self.cmd:
+          win32api.WinExec(self.cmd)
+    worker.join()
+
+if __name__=='__main__':
+  win32api.SetConsoleCtrlHandler(lambda event: True, True)  # keep running when logged off
+  win32serviceutil.HandleCommandLine(DoomsdayDevice)
